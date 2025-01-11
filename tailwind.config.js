70269dacb19599dbfe7e3c20abba7788378dacd8
Lutfan Hadi <hadilutfan3@gmail.com>
2025-01-11 19:06:27 +0700

    add index

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1C1E53",
+        button: "#FCD980",
+        layar: "#EEF4FA",
+        secondary: "#FFFFFF",
+        accent: "#282938",
+        card: "#F4F6FC",
+        border: "#ECECF1",
+        number: "#2405F2",
+        img: "#1C1E53",
+        width: {
+          405: "405px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
